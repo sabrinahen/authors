b555a1ea8e483533e1011f17e4a787194c314b10
Sabrina Henrice <sabrinahenrice@gmail.com>
2022-03-19 18:15:37 -0400

    adding oneauthor

--- client/src/components/DisplayOneAuthor.js
+++ client/src/components/DisplayOneAuthor.js
@@ -1,18 +1,51 @@
-import React, {useEffect, useState} from "react";
-import axios from "axios";
-import {Link, useNavigate} from "react-router-dom"
+// import React, {useEffect, useState} from "react";
+// import axios from "axios";
+// import {Link, useNavigate, useParams} from "react-router-dom"
 
-const DisplayOneAuthor = (props)=> {
+// const DisplayOneAuthor = (props)=> {
+
+//     const [author, setAuthor] = useState({});
+
+//     const {id} = useParams();
+
+//     const navigate = useNavigate();
+
+//     useEffect(()=>{
+//         axios.get(`http://localhost:8000/api/authors/${id}`)
+//             .then ((res)=> {
+//                 console.log(res);
+//                 console.log(res.data);
+//                 setAuthor(res.data)
+//             })
+//             .catch((err)=>{
+//                 console.log(err)
+//             })
+//     }, [id])
+
+//     const deleteOneAuthor = ()=>{
+//         axios.delete(`http://localhost:8000/api/authors/${id}`)
+//             .then((res)=>{
+//                 console.log(res);
+//                 console.log(res.data);
+//                 navigate("/")
+//             })
+//             .catch((err)=>{
+//                 console.log(err)
+//             })
+//     }
 
 
+//     return(
+//         <div>
+//             <header>
+//                 <h1>Favorite Authors</h1>
+//                 <Link to="/"><h4>Home</h4></Link>
+//             </header>
+//             <h4>Edit this author:</h4>
+//             <p>Name: {author.name}</p>
+//         </div>
+//     )
 
+// }
 
-    return(
-        <div>
-            
-        </div>
-    )
-
-}
-
-export default DisplayOneAuthor;
+// export default DisplayOneAuthor;
